b8fb6addeb89eeaeb604b5cd69c52447a4151620
solremi <jeremie.roc@gmail.com>
2024-07-03 12:05:40 +0200

    maj

--- client/components/Guestbook/Guestbook.tsx
+++ client/components/Guestbook/Guestbook.tsx
@@ -61,39 +61,41 @@ export default function Goldenbook() {
     return (
         <div className="goldenbook">
             <Header />
-            <div className="box hero-body has-background-warning-dark">
-                <h1 className="title is-2 has-text-white" id="title-gallery">Livre d'or</h1>
-                <p className="subtitle has-text-centered has-text-white">Écris-nous un petit mot, ça fait toujours plaisir</p>
-            </div>
-            <div id="container-message" className="container">
-                <form id="form-message" onSubmit={handleSubmit}>
-                    <textarea
-                        id="content"
-                        className="textarea is-warning"
-                        name="content"
-                        placeholder="Écris ici"
-                        value={content}
-                        onChange={(e) => setContent(e.target.value)}
-                        required
-                    />
-                    {error && <p className="has-text-danger">{error}</p>}
-                    <div className="control has-text-centered">
-                        <button type="submit" id="button-addMessage" className="button is-warning is-medium">
-                            Envoyer
-                        </button>
-                    </div>
-                </form>
-                <div>
-                    <p className="title is-4 has-text-warning-dark">Messages</p>
-                    <div id="messages" className="content">
-                        {messages.map((message, index) => (
-                            <div key={index} className="box">
-                                <p className="message-content">{message.content}</p>
-                                <p className="message-author">- {message.firstname}</p>
-                            </div>
-                        ))}
-                    </div>
+            <div id="container-global" className="container">
+                <div id="div-title" className="box hero-body">
+                    <h1 className="title is-2 has-text-white" id="title-gallery">Livre d'or</h1>
+                    <p className="subtitle has-text-centered has-text-white">Écris-nous un petit mot, ça fait toujours plaisir</p>
                 </div>
+                <div id="container-messageBox" className="container">
+                    <form id="form-message" onSubmit={handleSubmit}>
+                        <textarea
+                            id="content"
+                            className="textarea is-warning"
+                            name="content"
+                            placeholder="Écris ici"
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                            required
+                        />
+                        {error && <p className="has-text-danger">{error}</p>}
+                        <div className="control has-text-centered">
+                            <button type="submit" id="button-addMessage" className="button is-warning is-medium">
+                                Envoyer
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                    <div>
+                        <p className="title is-4">Messages</p>
+                        <div id="container-card" className="content">
+                            {messages.map((message, index) => (
+                                <div id="card" key={index} className="card mb-3">
+                                    <p className="message-content">{message.content}</p>
+                                    <p className="message-author"> {message.firstname}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
             </div>
             <Footer />
         </div>
--- server/datamappers/guestbook.datamapper.js
+++ server/datamappers/guestbook.datamapper.js
@@ -1,37 +1,37 @@
 
 class GuestbookDataMapper {
-  constructor(pool) {
-    this.pool = pool;
-  }
-
-  async addEntry({ content, userId }) {
-    const query = 'INSERT INTO "message" (content, user_id) VALUES ($1, $2) RETURNING *';
-    const values = [content, userId];
-
-    try {
-      const result = await this.pool.query(query, values);
-      return result.rows[0];
-    } catch (error) {
-      console.error('Error executing query:', error.message);
-      throw error;
+    constructor(pool) {
+      this.pool = pool;
+    }
+  
+    async addEntry({ content, userId }) {
+      const query = 'INSERT INTO "message" (content, user_id) VALUES ($1, $2) RETURNING *';
+      const values = [content, userId];
+  
+      try {
+        const result = await this.pool.query(query, values);
+        return result.rows[0];
+      } catch (error) {
+        console.error('Error executing query:', error.message);
+        throw error;
+      }
+    }
+  
+    async getEntries() {
+      const query = `
+        SELECT "message".content, "message".created_at, "user".firstname 
+        FROM "message" 
+        JOIN "user" ON "message".user_id = "user".id 
+        ORDER BY "message".created_at DESC
+      `;
+      try {
+        const result = await this.pool.query(query);
+        return result.rows;
+      } catch (error) {
+        console.error('Error executing query:', error.message);
+        throw error;
+      }
     }
   }
-
-  async getEntries() {
-    const query = `
-      SELECT message.content, message.created_at, user.firstname 
-      FROM message 
-      JOIN "user" ON message.user_id = "user".id 
-      ORDER BY message.created_at DESC
-    `;
-    try {
-      const result = await this.pool.query(query);
-      return result.rows;
-    } catch (error) {
-      console.error('Error executing query:', error.message);
-      throw error;
-    }
-  }
-}
-
-export default GuestbookDataMapper;
+  
+  export default GuestbookDataMapper;
