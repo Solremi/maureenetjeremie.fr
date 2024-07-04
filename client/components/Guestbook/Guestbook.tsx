8e85534c52ae74658e6be08108f3272ad558b80b
solremi <jeremie.roc@gmail.com>
2024-07-04 09:53:16 +0200

    maj

--- client/components/Guestbook/Guestbook.tsx
+++ client/components/Guestbook/Guestbook.tsx
@@ -6,7 +6,7 @@ import "./Guestbook.scss";
 
 export default function Goldenbook() {
     const [content, setContent] = useState("");
-    const [messages, setMessages] = useState([]);
+    const [messages, setMessages] = useState<{ content: string; firstname: string }[]>([]);
     const [error, setError] = useState("");
     const [userName, setUserName] = useState("");
 
@@ -61,41 +61,48 @@ export default function Goldenbook() {
     return (
         <div className="goldenbook">
             <Header />
-            <div id="container-global" className="container">
-                <div id="div-title" className="box hero-body">
-                    <h1 className="title is-2 has-text-white" id="title-gallery">Livre d'or</h1>
+            <div className="container">
+                <div id="div-title" className="box hero-body has-background-warning has-text-centered">
+                    <h1 className="title is-2 has-text-white">Livre d'or</h1>
                     <p className="subtitle has-text-centered has-text-white">Écris-nous un petit mot, ça fait toujours plaisir</p>
                 </div>
-                <div id="container-messageBox" className="container">
-                    <form id="form-message" onSubmit={handleSubmit}>
-                        <textarea
-                            id="content"
-                            className="textarea is-warning"
-                            name="content"
-                            placeholder="Écris ici"
-                            value={content}
-                            onChange={(e) => setContent(e.target.value)}
-                            required
-                        />
+                <div className="container">
+                    <form onSubmit={handleSubmit}>
+                        <div className="field">
+                            <div className="control">
+                                <textarea
+                                    className="textarea is-warning"
+                                    name="content"
+                                    placeholder="Écris ici"
+                                    value={content}
+                                    onChange={(e) => setContent(e.target.value)}
+                                    required
+                                />
+                            </div>
+                        </div>
                         {error && <p className="has-text-danger">{error}</p>}
                         <div className="control has-text-centered">
-                            <button type="submit" id="button-addMessage" className="button is-warning is-medium">
+                            <button type="submit" className="button is-warning is-medium">
                                 Envoyer
                             </button>
                         </div>
                     </form>
                 </div>
-                    <div>
-                        <p className="title is-4">Messages</p>
-                        <div id="container-card" className="content">
-                            {messages.map((message, index) => (
-                                <div id="card" key={index} className="card ">
-                                    <p className="message-content">{message.content}</p>
-                                    <p className="message-author"> {message.firstname}</p>
+                <div>
+                    <p className="title is-4">Messages</p>
+                    <div className="columns is-multiline">
+                        {messages.map((message, index) => (
+                            <div key={index} className="column is-one-third">
+                                <div className="card">
+                                    <div className="card-content">
+                                        <p className="message-content">{message.content}</p>
+                                        <p className="message-author has-text-right">{message.firstname}</p>
+                                    </div>
                                 </div>
-                            ))}
-                        </div>
+                            </div>
+                        ))}
                     </div>
+                </div>
             </div>
             <Footer />
         </div>
