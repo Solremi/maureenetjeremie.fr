110d3017e346815afd74f18e1f9e32ac8a7f5643
solremi <jeremie.roc@gmail.com>
2024-07-06 14:45:58 +0200

    maj

--- client/components/Place/Place.tsx
+++ client/components/Place/Place.tsx
@@ -7,11 +7,27 @@ export default function Place() {
     return (
         <div className="place">
             <Header />
-            <div id="container-page" className="container">
-                <div id="box" className="box hero-body">
+            <div id="contain-page" className="container">
+                <div id="box" className="box hero-body is-mobile ">
                     <h1 title="Lieu de la cérémonie" className="title is-1 has-text-white has-text-centered" id="title-gallery">
                         Informations utiles
                     </h1>
+                </div>
+               
+                    <div id="box-lieu" className="column is-full box">
+                        <h2 className="title">Lieu</h2>
+                        <p>51 Rue d'Anchin, 59146 Pecquencourt</p>
+                        <img src="/public/images/salleGoogle.png" alt="plan de la salle des fêtes" />
+                    </div>
+                    <div id="date-lieu" className="is-desktop">
+                    <div id="box-date" className="column is-full box">
+                        <h2 className="title">Date</h2>
+                        <p>Le 02 novembre 2024 - 15h</p>
+                        <p>Possibilité de se garer sur le parking.</p>
+                        <img src="/public/images/entreeSalle.png" alt="photo de la salle des fêtes" />
+                        <p>Ensuite, il y a deux entrées on commence à droite !</p>
+
+                    </div>
                 </div>
             </div>
             <Footer />
